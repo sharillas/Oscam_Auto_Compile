0fc1b8e2cd27794f1e231dd7d48408d8e3474be1
sharillas <ruperpost@hotmail.com>
2021-10-14 17:35:25 +0100

    Update oscamautocompile.sh

--- oscamautocompile.sh
+++ oscamautocompile.sh
@@ -1,16 +1,36 @@
-- Build your own Oscam (Ubuntu)
+# Build your own Oscam (Ubuntu & Debian)
 
+sudo -i
+apt update
 apt install -y cvs subversion autoconf libccid automake libtool gettext make cmake tar 
 apt install -y bzip2 gzip patch ncurses-bin gcc g++ flex bison pkg-config pcsc-tools pkg-config wget 
 apt install -y libpcsclite1 libpcsclite-dev libusb-dev libssl-dev libpnglite-dev pngquant zlib1g-dbg zlib1g-dev 
 apt install -y zlib1g zlibc dialog build-essential mercurial minizip
 apt install -y libsystemd-dev libudev-dev
 
-svn checkout https://svn.streamboard.tv/oscam/trunk oscam-svn
+# Install PCSC Support
+wget https://pcsclite.apdu.fr/files/pcsc-lite-1.9.0.tar.bz2
+tar -xf pcsc-lite-1.9.0.tar.bz2
+cd pcsc-lite-1.9.0
+./configure
+make
+sudo make install
 
+# Install LIBUSB Support
+wget https://github.com/libusb/libusb/releases/download/v1.0.23/libusb-1.0.23.tar.bz2
+tar -xf libusb-1.0.23.tar.bz2
+cd libusb-1.0.23
+./configure
+make
+sudo make install
+
+# Download and Cross-Compile Oscam last build from SVN Streamboard
+svn checkout https://svn.streamboard.tv/oscam/trunk oscam-svn
 cd oscam-svn
 mkdir build
 cd build
 cmake ..
 make
 make install
+
+# O Novo Oscam Compilado estara localizado em /usr/local/bin
