7be41caea6c5db03fa30acd406bab59200d4b638
sharillas <ruperpost@hotmail.com>
2021-10-14 16:12:42 +0100

    Update oscamautocompile.sh

--- oscamautocompile.sh
+++ oscamautocompile.sh
@@ -1,6 +1,10 @@
 - Build your own Oscam (Ubuntu)
 
-apt-get install cvs subversion autoconf libccid automake libtool gettext make cmake tar bzip2 gzip patch ncurses-bin gcc g++ flex bison pkg-config pcsc-tools pkg-config wget libpcsclite1 libpcsclite-dev libusb-dev libssl-dev libpnglite-dev pngquant zlib1g-dbg zlib1g-dev zlib1g zlibc dialog build-essential mercurial minizip
+apt install -y cvs subversion autoconf libccid automake libtool gettext make cmake tar 
+apt install -y bzip2 gzip patch ncurses-bin gcc g++ flex bison pkg-config pcsc-tools pkg-config wget 
+apt install -y libpcsclite1 libpcsclite-dev libusb-dev libssl-dev libpnglite-dev pngquant zlib1g-dbg zlib1g-dev 
+apt install -y zlib1g zlibc dialog build-essential mercurial minizip
+apt install -y libsystemd-dev libudev-dev
 
 svn checkout https://svn.streamboard.tv/oscam/trunk oscam-svn
 
