9dc9f5417f2380987a3af92c9cd27eb2b59b2020
sharillas <ruperpost@hotmail.com>
2022-03-21 17:57:36 +0000

    Update oscamautocompile.sh

--- oscamautocompile.sh
+++ oscamautocompile.sh
@@ -7,9 +7,8 @@ apt install bzip2 gzip patch ncurses-bin gcc g++ flex bison pkg-config pkg-confi
 apt install libusb-dev libssl-dev libpnglite-dev pngquant zlib1g-dbg zlib1g-dev 
 apt install zlib1g zlibc dialog build-essential mercurial minizip
 apt install libsystemd-dev libudev-dev
-
-
-# Download and Cross-Compile Oscam last build from SVN Streamboard
+sleep 5
+echo "Download and Cross-Compile Oscam last build from SVN Streamboard"
 svn checkout https://svn.streamboard.tv/oscam/trunk oscam-svn
 sleep 3
 cd oscam-svn
@@ -17,5 +16,4 @@ mkdir build
 cd build
 cmake ..
 make install
-
-# O Novo Oscam Compilado estara localizado em /usr/local/bin
+echo" O Novo Oscam Compilado estara localizado em /usr/local/bin"
